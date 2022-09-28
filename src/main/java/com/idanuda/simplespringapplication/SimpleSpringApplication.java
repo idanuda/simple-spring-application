f4374343e44c349a33bb1914e8f16a2ef52c0743
idanyehuda <90613271+idanyehuda@users.noreply.github.com>
2022-09-28 11:49:27 +0300

    Initial commit

---
+++ src/main/java/com/idanuda/simplespringapplication/SimpleSpringApplication.java
@@ -0,0 +1,19 @@
+package com.idanuda.simplespringapplication;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SimpleSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringApplication.class, args);
+    }
+
+
+}
